0e128305a701ebbc940f48f38ff3e1d84a3fd547
eldritchtools <eldritch.tools@gmail.com>
2025-09-28 14:40:59 +0800

    Fix export names

--- dist/data/mdData.js
+++ dist/data/mdData.js
@@ -1,3 +1,3 @@
-import md_floor_packs from './md_floor_packs.json';
-import md_theme_packs from './md_theme_packs.json';
-export { md_floor_packs, md_theme_packs };
+import floorPacks from './md_floor_packs.json';
+import themePacks from './md_theme_packs.json';
+export { floorPacks, themePacks };
--- src/data/mdData.js
+++ src/data/mdData.js
@@ -1,4 +1,4 @@
-import md_floor_packs from './md_floor_packs.json';
-import md_theme_packs from './md_theme_packs.json';
+import floorPacks from './md_floor_packs.json';
+import themePacks from './md_theme_packs.json';
 
-export {md_floor_packs, md_theme_packs};
+export {floorPacks, themePacks};
