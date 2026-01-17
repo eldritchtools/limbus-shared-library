4c682b5033d9a470b2827a56b591fe79a8fae91a
eldritchtools <eldritch.tools@gmail.com>
2026-01-18 04:04:42 +0800

    Fix the path to public root for metadata

--- src/paths.js
+++ src/paths.js
@@ -1,5 +1,5 @@
 const ASSETS_ROOT = "https://limbus-assets.eldritchtools.com/assets";
 const DATA_ROOT = "https://limbus-assets.eldritchtools.com/data";
-const PUBLIC_ROOT = "https://limbus-assets.eldritchtools.com/";
+const PUBLIC_ROOT = "https://limbus-assets.eldritchtools.com";
 
 export { ASSETS_ROOT, DATA_ROOT, PUBLIC_ROOT };
