c0fc0145ce7f2400bb92ad604c8fb914b5aa1025
eldritchtools <eldritch.tools@gmail.com>
2026-02-21 00:59:11 +0800

    Reduce max tooltip width

--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,12 @@
-const tooltipStyle = { outlineStyle: "solid", outlineColor: "#ddd", outlineWidth: "1px", backgroundColor: "#000000", borderRadius: "1rem", zIndex: "9999", maxWidth: "90vw" };
+const tooltipStyle = { 
+    outlineStyle: "solid", 
+    outlineColor: "#ddd", 
+    outlineWidth: "1px", 
+    backgroundColor: "#000000", 
+    borderRadius: "1rem", 
+    zIndex: "9999", 
+    width: "90%",
+    maxWidth: "800px" 
+};
 
 export { tooltipStyle };
